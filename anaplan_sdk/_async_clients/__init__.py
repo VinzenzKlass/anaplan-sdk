255e2f0ba46dea0a86047e34f5c145c353b7c82b
Vinzenz Klass <vinzenz.klass@ba.valantic.com>
2025-03-28 14:13:20 +0100

    feat: list tasks

--- anaplan_sdk/_async_clients/__init__.py
+++ anaplan_sdk/_async_clients/__init__.py
@@ -1,5 +1,6 @@
 from ._alm import _AsyncAlmClient
+from ._audit import _AsyncAuditClient
 from ._bulk import AsyncClient
 from ._transactional import _AsyncTransactionalClient
 
-__all__ = ["_AsyncAlmClient", "AsyncClient", "_AsyncTransactionalClient"]
+__all__ = ["AsyncClient", "_AsyncAlmClient", "_AsyncAuditClient", "_AsyncTransactionalClient"]
--- anaplan_sdk/_clients/__init__.py
+++ anaplan_sdk/_clients/__init__.py
@@ -1,5 +1,6 @@
 from ._alm import _AlmClient
+from ._audit import _AuditClient
 from ._bulk import Client
 from ._transactional import _TransactionalClient
 
-__all__ = ["_AlmClient", "Client", "_TransactionalClient"]
+__all__ = ["Client", "_AlmClient", "_AuditClient", "_TransactionalClient"]
