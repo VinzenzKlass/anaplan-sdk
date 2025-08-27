57b3b5348ac501cd41ac13f20fd3005035f71bcd
Vinzenz Klass <vinzenz.klass@valantic.com>
2025-08-27 15:26:59 +0200

    chore: docstrings

--- anaplan_sdk/_async_clients/_scim.py
+++ anaplan_sdk/_async_clients/_scim.py
@@ -22,18 +22,43 @@ class _AsyncScimClient:
         self._url = "https://api.anaplan.com/scim/1/0/v2"
 
     async def get_service_provider_config(self) -> ServiceProviderConfig:
+        """
+        Get the SCIM Service Provider Configuration.
+        :return: The ServiceProviderConfig object describing the available SCIM features.
+        """
         res = await self._http.get(f"{self._url}/ServiceProviderConfig")
         return ServiceProviderConfig.model_validate(res)
 
     async def get_resource_types(self) -> list[Resource]:
+        """
+        Get the SCIM Resource Types.
+        :return: A list of Resource objects describing the SCIM resource types.
+        """
         res = await self._http.get(f"{self._url}/ResourceTypes")
         return [Resource.model_validate(e) for e in res.get("Resources", [])]
 
     async def get_resource_schemas(self) -> list[Schema]:
+        """
+        Get the SCIM Resource Schemas.
+        :return: A list of Schema objects describing the SCIM resource schemas.
+        """
         res = await self._http.get(f"{self._url}/Schemas")
         return [Schema.model_validate(e) for e in res.get("Resources", [])]
 
     async def get_users(self, predicate: str | field = None, page_size: int = 100) -> list[User]:
+        """
+        Get a list of users, optionally filtered by a predicate. Keep in mind that this will only
+        return internal users. To get a list of all users in the tenant, use the `get_users()`
+        in the `audit` namespace instead.
+        :param predicate: A filter predicate to filter the users. This can either be a string,
+               in which case it will be passed as-is, or an expression. Anaplan supports filtering
+               on the following fields: "id", "externalId", "userName", "name.familyName",
+               "name.givenName" and "active". It supports the operators "eq", "ne", "gt", "ge",
+               "lt", "le" and "pr". It supports logical operators "and" and "or", "not" is not
+               supported. It supports grouping with parentheses.
+        :param page_size: The number of users to fetch per page. Values above 100 will error.
+        :return: The internal users optionally matching the filter.
+        """
         params: dict[str, int | str] = {"startIndex": 1, "count": page_size}
         if predicate is not None:
             params["filter"] = str(predicate)
@@ -54,14 +79,35 @@ class _AsyncScimClient:
         return users
 
     async def get_user(self, user_id: str) -> User:
+        """
+        Get a user by their ID.
+        :param user_id: The ID of the user to fetch.
+        :return: The User object.
+        """
         res = await self._http.get(f"{self._url}/Users/{user_id}")
         return User.model_validate(res)
 
     async def add_user(self, user: UserInput | dict[str, Any]) -> User:
+        """
+        Add a new user to your Anaplan tenant.
+        :param user: The user info to add. Can either be a UserInput object or a dict. If you pass
+               a dict, it will be validated against the UserInput model before sending. If the info
+               you provided is invalid or incomplete, this will raise a pydantic.ValidationError.
+        :return: The created User object.
+        """
         res = await self._http.post(f"{self._url}/Users", json=construct_payload(UserInput, user))
         return User.model_validate(res)
 
     async def replace_user(self, user_id: str, user: ReplaceUserInput | dict[str, Any]):
+        """
+        Replace an existing user with new information. Note that this will replace all fields of the
+        :param user_id: ID of the user to replace.
+        :param user: The new user info. Can either be a ReplaceUserInput object or a dict. If you
+               pass a dict, it will be validated against the ReplaceUserInput model before sending.
+               If the info you provided is invalid or incomplete, this will raise a
+               pydantic.ValidationError.
+        :return: The updated User object.
+        """
         res = await self._http.put(
             f"{self._url}/Users/{user_id}", json=construct_payload(ReplaceUserInput, user)
         )
@@ -70,6 +116,17 @@ class _AsyncScimClient:
     async def update_user(
         self, user_id: str, operations: list[Operation] | list[dict[str, Any]]
     ) -> User:
+        """
+        Update an existing user with a list of operations. This allows you to update only specific
+        fields of the user without replacing the entire user.
+        :param user_id: The ID of the user to update.
+        :param operations: A list of operations to perform on the user. Each operation can either be
+               an Operation object or a dict. If you pass a dict, it will be validated against
+               the Operation model before sending. If the operation is invalid, this will raise a
+               pydantic.ValidationError. You can also use the models Replace, Add and Remove which
+               are subclasses of Operation and provide a more convenient way to create operations.
+        :return: The updated User object.
+        """
         res = await self._http.patch(
             f"{self._url}/Users/{user_id}",
             json={
